23ef19929a99b0a61d2ba841711d90a7ee7912ae
ignat <ignatpetrenko@gmail.com>
2019-02-26 13:37:54 +0300

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'build')
   },
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 }
